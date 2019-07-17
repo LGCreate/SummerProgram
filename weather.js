8de0a2aeab8b6e0d7340f4fcc98b38f71c5f414e
Liesel Clark <lieselsstorybook@gmail.com>
2019-07-16 20:29:59 -0600

    added today weather

--- weather.js
+++ weather.js
@@ -1,22 +1,38 @@
-var weatherData;
-var request = new XMLHttpRequest();
-var date = new Date();
 
-loadData();
-
-function loadData() {
-    
-    request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Salt+Lake+City,us&appid=b12b9ec8e6619040130f8e8773e1a668');
-    request.onload = loadComplete;
+function loadTodaysWeather() {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Salt+Lake+City,us&units=imperial&appid=b12b9ec8e6619040130f8e8773e1a668');
+    request.onload = function (evt) {
+        let date = new Date();
+        let  weatherData = JSON.parse(request.responseText);
+        console.log(weatherData);
+        document.getElementById("place").innerHTML = weatherData.name;
+        document.getElementById("day").innerHTML = (date.getMonth()+1) + "/" + date.getDate();
+        document.getElementById("currentTemp").innerHTML = weatherData.main.temp;
+        document.getElementById("conditions").innerHTML = weatherData.weather[0].main;
+        document.getElementById("conditionsDesc").innerHTML = weatherData.weather[0].description;  
+    };
     request.send();
 }
 
-function loadComplete(evt) {
-    weatherData = JSON.parse(request.responseText);
-    console.log(weatherData);
-    document.getElementById("place").innerHTML = weatherData.city.name;
-    document.getElementById("day").innerHTML = (date.getMonth()+1) + "/" + date.getDate();
-    document.getElementById("currentTemp").innerHTML = weatherData.list[0].temp.day;
-    document.getElementById("conditions").innerHTML = weatherData.list[0].weather[0].main;
-    document.getElementById("conditionsDesc").innerHTML = weatherData.list[0].weather[0].description;  
+function loadFiveDayForecast() {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?q=Salt+Lake+City,us&units=imperial&appid=b12b9ec8e6619040130f8e8773e1a668');
+    request.onload = function (evt) {
+        let weatherData = JSON.parse(request.responseText);
+        console.log(weatherData);
+
+        
+        // document.getElementById("place").innerHTML = weatherData.name;
+        // document.getElementById("day").innerHTML = (date.getMonth()+1) + "/" + date.getDate();
+        // document.getElementById("currentTemp").innerHTML = weatherData.main.temp;
+        // document.getElementById("conditions").innerHTML = weatherData.weather[0].main;
+        // document.getElementById("conditionsDesc").innerHTML = weatherData.weather[0].description;  
+    };
+    request.send();
 }
+
+
+
+loadTodaysWeather();
+loadFiveDayForecast();
