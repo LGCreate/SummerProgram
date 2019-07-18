a600140981799de0b6ef3a4155d5a754658ed027
Liesel Clark <lieselsstorybook@gmail.com>
2019-07-17 21:21:43 -0600

    did parcial forcast

--- weather.js
+++ weather.js
@@ -22,12 +22,14 @@ function loadFiveDayForecast() {
         let weatherData = JSON.parse(request.responseText);
         console.log(weatherData);
 
-        
+
+        for (let counter = 0; counter < weatherData.list.length; counter++) {
         // document.getElementById("place").innerHTML = weatherData.name;
         // document.getElementById("day").innerHTML = (date.getMonth()+1) + "/" + date.getDate();
         // document.getElementById("currentTemp").innerHTML = weatherData.main.temp;
         // document.getElementById("conditions").innerHTML = weatherData.weather[0].main;
-        // document.getElementById("conditionsDesc").innerHTML = weatherData.weather[0].description;  
+        // document.getElementById("conditionsDesc").innerHTML = weatherData.weather[0].description;
+        }
     };
     request.send();
 }
